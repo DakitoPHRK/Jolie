3f4927849a00dd40b496d6deaf5be7fd4f457fa5
DakitoPHRK <109562454+DakitoPHRK@users.noreply.github.com>
2024-12-30 14:22:12 -0300

    Update dak.sh

--- dak.sh
+++ dak.sh
@@ -1,25 +1,32 @@
 /interface wireguard add listen-port=2408 mtu=1420 name=wireguard1 private-key="kLaCS+syC4OKKrWS4NlOGgbIoXnQo9sHYCukhcIUOkk="
-sleep 2
+
+:delay 2
 /interface wireguard peers add allowed-address=0.0.0.0/0 endpoint-address=162.159.192.1 endpoint-port=2408 interface=wireguard1 persistent-keepalive=25 public-key="bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo="
-sleep 2
+
+:delay 2
 /ip address add address=172.16.0.2/32 interface=wireguard1
-sleep 2
+
+:delay 2
 /ip route add distance=1 gateway=wireguard1 dst-address=0.0.0.0/0
-sleep 2
+
+:delay 2
 /ip dns set servers=1.1.1.1
-sleep 2
+
+:delay 2
 /ip firewall nat add chain=srcnat action=masquerade out-interface=wireguard1
+
 /interface wireguard print
-sleep 2
+:delay 2
 /interface wireguard peers print
-sleep 2
+:delay 2
 /interface lte print
 
 /ip route print
-sleep 2
+:delay 2
 /ip route remove [find dst-address=0.0.0.0/0 gateway=wireguard1]
-sleep 2
+:delay 2
 /ip route add dst-address=0.0.0.0/0 gateway=wireguard1 distance=1
-sleep 2
+:delay 2
 /ip route add dst-address=162.159.192.1 gateway=lte1 distance=1
-echo "Finalizado."
+
+:log info "Finalizado."
